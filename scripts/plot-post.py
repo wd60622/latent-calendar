02b36e9d27d25631460122e3d87b2c08a949964e
Will Dean <wd60622@gmail.com>
2024-01-31 07:46:32 +0100

    Add wrapper for the series

--- scripts/plot-post.py
+++ scripts/plot-post.py
@@ -13,26 +13,20 @@ if __name__ == "__main__":
     df = pd.read_csv(file)
 
     # Create a datetime column
-    df["datetime"] = pd.to_datetime(df["date"].str.cat(df["time"], sep=" "))
+    datetime_column = pd.to_datetime(df["date"].str.cat(df["time"], sep=" "))
 
 
     fig, axes = plt.subplots(nrows=1, ncols=2, sharey=True)
     fig.suptitle("Plotting datetime column from posit-dev/great-tables pizza place dataset")
 
     ax = axes[0]
-    df["datetime"].sample(n=100, random_state=0).cal.plot(ax=ax)
+    datetime_column.sample(n=100, random_state=0).cal.plot(ax=ax)
     ax.set_title("Continuous Series cal.plot()")
 
     ax = axes[1]
     (
-        df
-        .assign(tmp=1)
-        .cal.aggregate_events(
-            by="tmp", 
-            timestamp_col="datetime", 
-            minutes=30
-        )
-        .iloc[0]
+        datetime_column
+        .cal.aggregate_events(minutes=30)
         .cal.plot_row(ax=ax)
     )
     ax.set_title("Discretized Series cal.plot_row()")
